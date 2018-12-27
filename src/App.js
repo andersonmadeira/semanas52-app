6e2fb15ebaf4afbf6ee5f0416bbee15039003912
Anderson Madeira <andersonmadeiracs@gmail.com>
2018-12-27 11:05:31 -0300

    Displaying week graph

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { AnimatedCircularProgress } from 'react-native-circular-progress';
 
 function Greeting(props) {
   return (
@@ -11,16 +12,46 @@ function Greeting(props) {
 }
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    let startWeek = 40;
+
+    this.state = { currentWeek: startWeek, percent: startWeek / 52 * 100 };
+
+    this.getPercentualFromWeek = this.getPercentualFromWeek.bind(this);
+  }
+
+  getPercentualFromWeek(weekNumber) {
+    return (weekNumber / 52) * 100;
+  }
+
   render() {
     return (
       <View style={styles.page}>
         <View style={styles.topIconBar}>
           <View style={styles.leftButtons}>
-            <Icon name='info-circle' style={styles.topButton} size={50}></Icon>
+            <Icon name='info-circle' style={styles.topButton} size={40}></Icon>
           </View>
           <View style={styles.rightButtons}>
-            <Icon name='cog' style={styles.topButton} size={50}></Icon>
+            <Icon name='cog' style={styles.topButton} size={40}></Icon>
           </View>
+        </View>
+        <View style={styles.graphContainer}>
+          <AnimatedCircularProgress
+            size={250}
+            width={35}
+            fill={this.state.percent}
+            tintColor="#fff"
+            backgroundColor="#689F38">
+            {
+              (fill) => (
+                <Text style={styles.labelWeeks}>
+                  Semana{'\n'}{ this.state.currentWeek }
+                </Text>
+              )
+            }
+          </AnimatedCircularProgress>
         </View>
         <Greeting name='Anderson'></Greeting>
         <Greeting name='Emanuelle'></Greeting>
@@ -33,7 +64,7 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   page: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: '#8BC34A',
   },
   topIconBar: {
     padding: 15,
@@ -52,5 +83,13 @@ const styles = StyleSheet.create({
   },
   topButton: {
     color: '#fff',
+  },
+  graphContainer: {
+    alignItems: 'center',
+  },
+  labelWeeks: {
+    color: '#497127',
+    fontSize: 30,
+    textAlign: 'center',
   }
 })
