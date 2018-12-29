d1ab19312727ddf097b3612d7c87ccde92a2b1dc
Anderson Madeira <andersonmadeiracs@gmail.com>
2018-12-29 17:44:08 -0300

    WIP: Tela de config

--- src/pages/config-page.js
+++ src/pages/config-page.js
@@ -1,14 +1,69 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import { connect } from 'react-redux';
+import { updateStartDate, updateStartAmount } from '../actions';
+import { bindActionCreators } from 'redux';
 
-export default class ConfigPage extends Component {
+class ConfigPage extends Component {
     static navigationOptions = {
-        title: 'Config'
+        title: 'Details'
     };
 
     render() {
+        const { updateStartAmount, updateStartDate, amount, date } = this.props;
+
         return (
-            <Text>Config Page</Text>
+            <View style={styles.page}>
+                <View style={styles.topActionBar}>
+                    <TouchableOpacity style={styles.backActionContainer} onPress={() => this.props.navigation.goBack()}>
+                        <Icon name={'chevron-left'} style={[styles.topTextFont, styles.backIcon]} size={25} />
+                        <Text style={[styles.topTextFont, styles.backLabel]}>Voltar</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.configPageWrapper}>
+                    <Text>Valor Inicial:</Text>
+                    <TextInput keyboardType='numeric' placeholder='Valor inicial' onChangeText={(amount) => updateStartAmount(amount)} value={`${amount}`} />
+                    <Text>Data de início:</Text>
+                    <TextInput placeholder='Valor inicial' onChangeText={(date) => updateStartDate(date)} value={date} />
+                </View>
+            </View>
         );
     }
 }
+
+const mapStateToProps = store => ({
+    amount: store.amountState.amount,
+    date: store.dateState.date
+});
+
+const mapDispatchToProps = dispatch =>
+    bindActionCreators({ updateStartDate, updateStartAmount }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(ConfigPage);
+
+const styles = StyleSheet.create({
+    page: {
+        flex: 1,
+        backgroundColor: '#8BC34A',
+    },
+    topActionBar: {
+        padding: 15,
+    },
+    backActionContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    topTextFont: {
+        color: '#fff',
+    },
+    backLabel: {
+        paddingLeft: 15,
+        fontSize: 20
+    },
+    configPageWrapper: {
+        flex: 1,
+        flexDirection: 'column',
+        padding: 50
+    }
+});
--- src/pages/details-page.js
+++ src/pages/details-page.js
@@ -1,33 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput } from 'react-native';
-import { connect } from 'react-redux';
-import { updateStartDate, updateStartAmount } from '../actions';
-import { bindActionCreators } from 'redux';
+import { Text } from 'react-native';
 
-class DetailsPage extends Component {
+export default class DetailsPage extends Component {
     static navigationOptions = {
-        title: 'Details'
+        title: 'Config'
     };
 
     render() {
-        const { updateStartAmount, updateStartDate, amount, date } = this.props;
-
         return (
-            <View>
-                <Text>Details page</Text>
-                <TextInput keyboardType='numeric' placeholder='Valor inicial' onChangeText={(amount) => updateStartAmount(amount)} value={`${amount}`} />
-                <TextInput placeholder='Valor inicial' onChangeText={(date) => updateStartDate(date)} value={date} />
-            </View>
+            <Text>Details Page</Text>
         );
     }
 }
-
-const mapStateToProps = store => ({
-    amount: store.amountState.amount,
-    date: store.dateState.date
-});
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators({ updateStartDate, updateStartAmount }, dispatch);
-
-export default connect(mapStateToProps, mapDispatchToProps)(DetailsPage);
